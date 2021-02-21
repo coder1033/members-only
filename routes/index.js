25a8a5f17928c8f9f28601ec54ecfc41167ebd97
Manan Jain <manan09mj@gmail.com>
2021-02-22 02:10:05 +0530

    index.js add auth route for sign-up and log-in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+// npm imports
+const express = require("express");
+
+// local imports
+const authRouter = require("./auth");
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.use("/auth", authRouter);
 
 module.exports = router;
