12bd093681db23de35e6be34f0254ce806aaae4a
Manan Jain <manan09mj@gmail.com>
2021-02-22 01:36:51 +0530

    created a user model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, minlength: 3, maxlength: 50 },
+  last_name: { type: String, required: true, minlength: 3, maxlength: 50 },
+  username: { type: String, required: true, minlength: 3, maxlength: 30 },
+  password: { type: String, required: true, minlength: 8, maxlength: 50 },
+  membership: { type: Boolean, default: false },
+  admin: {type: Boolean, default: false}
+});
+
+module.exports = mongoose.model("User", UserSchema);
