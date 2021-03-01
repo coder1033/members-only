6476a05548d863f0ceb0d58be448330b91fa6d5d
Manan Jain <manan09mj@gmail.com>
2021-03-01 22:29:20 +0530

    user model update maxlength upto 128 chars

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
   first_name: { type: String, required: true, minlength: 3, maxlength: 50 },
   last_name: { type: String, required: true, minlength: 3, maxlength: 50 },
   username: { type: String, required: true, minlength: 3, maxlength: 30 },
-  password: { type: String, required: true, minlength: 8, maxlength: 50 },
+  password: { type: String, required: true, minlength: 8, maxlength: 128 },
   membership: { type: Boolean, default: false },
   admin: {type: Boolean, default: false}
 });
