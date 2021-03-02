22d7ab9a15b3f1ff63701eac1801e0867bbee5e7
Manan Jain <manan09mj@gmail.com>
2021-03-02 14:02:58 +0530

    message model change field name message to description

--- models/message.js
+++ models/message.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
   title: { type: String, required: true, minlength: 3, maxlength: 50 },
-  message: { type: String, required: true, minlength: 1, maxlength: 50 },
+  description: { type: String, required: true, minlength: 1, maxlength: 500 },
   date: { type: Date, default: Date.now },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
