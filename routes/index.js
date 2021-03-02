855bd4c6098b0103a5a0779c0d03881115e68bfa
Manan Jain <manan09mj@gmail.com>
2021-03-02 14:10:37 +0530

    index.js adding messages and membership routers

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,58 @@
 // npm imports
 const express = require("express");
 
-// local imports
+// require models
+const Message = require("../models/message");
+const User = require("../models/user");
+
+// require routers
 const authRouter = require("./auth");
+const messagesRouter = require("./messages");
+
+// environment variables
+require("dotenv").config();
 
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  Message.find({}, function (err, messages) {
+    if (err) {
+      return next(err);
+    }
+    res.render("index", { title: "ChatHouse", messages });
+  });
 });
 
 router.use("/auth", authRouter);
+
+router.use("/messages", messagesRouter);
+
+router.get("/membership", function (req, res, next) {
+  res.render("membership", { title: "Membership" });
+});
+
+router.post("/membership", function (req, res, next) {
+  const { membership_passcode } = req.body;
+  const { _id } = req.user;
+
+  if (membership_passcode == process.env.MEMBERSHIP_PASSCODE) {
+    User.findByIdAndUpdate(
+      { _id },
+      { membership: true },
+      function (err, updated_user) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/");
+      }
+    );
+  } else {
+    res.render("membership", {
+      title: "Membership",
+      errors: ["Incorrect Passcode"],
+    });
+  }
+});
 
 module.exports = router;
